78b5427601e94401a4829432481c46443512fd57
Avi <67220821+avi-lime@users.noreply.github.com>
2020-07-28 21:49:08 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,12 +9,31 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    if (!msg.content.startsWith === prefix) return;
-    const command = msg.content.slice(prefix.length);
+    if (!msg.content.startsWith === prefix || msg.author.bot) return;
+
+    const args = msg.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
     if (command === 'ping') {
         msg.reply('pong!');
     }
 
+    if (command === 'avatar' || command === 'av') {
+        if (!msg.content.users.size) {
+            const av = new Discord.MessageEmbed()
+                .setTitle(msg.author.name + '\'s avatar!')
+                .setImage(msg.author.displayAvatarURL({ dynamic: true, format: "png", size: 4096 }))
+                .setColor(msg.author.displayHexColor())
+                .setThumbnail(guild.iconURL());
+            msg.channel.send(av);
+        }
+        const userav = new Discord.MessageEmbed()
+            .setTitle(msg.users.mentions.first() + '\'s avatar!')
+            .setImage(msg.users.mentions.first.displayAvatarURL({ dynamic: true, format: 'png', size: 4096 }))
+            .setColor(msg.users.mentions.first.displayHexColor())
+            .setThumbnail(guild.iconURL());
+        msg.channel.send(userav);
+    }
 });
 
 client.login(process.env.token);
