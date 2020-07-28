a1acb596f333bca80032c0c6c57865b9fedcc852
Avi <67220821+avi-lime@users.noreply.github.com>
2020-07-28 21:15:33 +0530

    test prefix

--- bot.js
+++ bot.js
@@ -2,15 +2,19 @@ require('dotenv').config();
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = "=";
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-    if (msg.content === '=ping') {
+    if (!msg.content.startsWith === prefix) return;
+    const command = msg.content.slice(prefix.length);
+    if (command === 'ping') {
         msg.reply('pong!');
     }
+
 });
 
 client.login(process.env.token);
