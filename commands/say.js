3441f41e0367c445da9bf73bfd3ceb801b9ce36b
Avi <67220821+avi-lime@users.noreply.github.com>
2020-07-31 16:19:59 +0530

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,13 +1,14 @@
 const { commands } = require("npm");
 
 const prefix = '=';
-const args = msg.content.slice(prefix.length).trim().split(/ +/);
-const commandName = args.shift().toLowerCase();
 
 module.exports = {
     name: 'say',
     description: 'make the bot say something',
     execute(msg, args) {
+        const args = msg.content.slice(prefix.length).trim().split(/ +/);
+        const commandName = args.shift().toLowerCase();
+
         if (!args.length) {
             return msg.channel.send('provide an arguement for me to say.');
         }
