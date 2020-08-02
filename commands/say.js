6337cbcd9938a97bcbbc3c23c50994f9d185277b
Avi <67220821+avi-lime@users.noreply.github.com>
2020-08-02 15:12:41 +0530

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -6,12 +6,13 @@ module.exports = {
     name: 'say',
     description: 'make the bot say something',
     execute(msg) {
-        if (!msg.author.id === '454307252392951819') return;
-
         const args = msg.content.slice(prefix.length).trim().split(/ +/);
         const commandName = args.shift().toLowerCase();
-        const mentionedRole = msg.mentions.roles;
-        const says = msg.content.replace(prefix + commandName, " ").replace(mentionedRole, mentionedRole.toString());
+        const mentionedRoles = msg.mentions.roles;
+        const says = msg.content.replace(prefix + commandName, " ");
+        if (mentionedRoles) {
+            const says = msg.content.removeMentions(says);
+        }
         if (!args.length) {
             return msg.channel.send('provide an arguement for me to say.');
         }
