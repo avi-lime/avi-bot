e3226c87106a8eba1991352c5366085c79c18218
Avi <67220821+avi-lime@users.noreply.github.com>
2020-08-12 15:30:28 +0530

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -8,12 +8,13 @@ module.exports = {
     execute(msg) {
         const args = msg.content.slice(prefix.length).trim().split(/ +/);
         const commandName = args.shift().toLowerCase();
-        const mentions = /\<\@\!?\&?([0-9]|everyone|here)\>/
+        if (msg.content.match(/\<\@\&?([0-9]|everyone|here)\>/)) {
+            return msg.content.send('remove the mentions from the message, this is to prevent spam, sorry');
+        }
         const says = msg.content.replace(prefix + commandName, " ");
         if (!args.length) {
             return msg.channel.send('provide an arguement for me to say.');
         }
         msg.delete();
-        msg.channel.send(says.replace(mentions, ""));
     },
 };
