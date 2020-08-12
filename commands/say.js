3b7d19d4894879f379978d72262441e7c5725e05
Avi <67220821+avi-lime@users.noreply.github.com>
2020-08-12 15:20:58 +0530

    idek what i am doing

--- commands/say.js
+++ commands/say.js
@@ -8,11 +8,12 @@ module.exports = {
     execute(msg) {
         const args = msg.content.slice(prefix.length).trim().split(/ +/);
         const commandName = args.shift().toLowerCase();
+        const mentions = /\<\@\!?\&?([0-9]|everyone|here)\>/
         const says = msg.content.replace(prefix + commandName, " ");
         if (!args.length) {
             return msg.channel.send('provide an arguement for me to say.');
         }
-        msg.delete().then
-        msg.channel.send.removeMentions(says);
+        msg.delete();
+        msg.channel.send(says.replace(mentions, ""));
     },
 };
