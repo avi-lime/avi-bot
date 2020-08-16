02ed81b9f46d27f0d7ca68a462eacce7a58c07e7
Avi <67220821+avi-lime@users.noreply.github.com>
2020-08-16 20:19:41 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 const Discord = require('discord.js');
 const client = new Discord.Client({
-    disableMentions: "all"
+    disableMentions: "everyone"
 });
 const prefix = "=";
 
