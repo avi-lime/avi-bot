85bd2f84a71c93d6e80c38d72ea2a7c85f783dbc
Avi <67220821+avi-lime@users.noreply.github.com>
2020-08-16 12:57:35 +0530

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -1,8 +1,8 @@
 module.exports = {
     name: 'reload',
     description: 'Reloads a command',
-    args: true,
     execute(message, args) {
+        if (!args.length) return message.channel.send(`You didn't pass any command to reload, ${message.author}!`);
         const commandName = args[0].toLowerCase();
         const command = message.client.commands.get(commandName)
             || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
